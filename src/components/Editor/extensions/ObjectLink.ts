2d8a7ca9c8af418ce9e87518b833ad0f235220f8
Alberto Massia <mcmassia@McBookPro.local>
2026-02-05 17:24:08 +0100

    fix(editor): allow standard links in objectLink extension

--- src/components/Editor/extensions/ObjectLink.ts
+++ src/components/Editor/extensions/ObjectLink.ts
@@ -69,6 +69,9 @@ export const ObjectLink = Link.extend({
             {
                 tag: 'a[href^="object:"]',
             },
+            {
+                tag: 'a[href]',
+            },
         ];
     },
 
